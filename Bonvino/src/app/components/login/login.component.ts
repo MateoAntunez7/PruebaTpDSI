4a0d646fd998377424c9dfd9a13bd4d0470a60fa
MateoAntunez7 <mateoantunez0@gmail.com>
2024-05-05 11:57:00 -0300

    Login

---
+++ Bonvino/src/app/components/login/login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'] // Usa 'styleUrls' en lugar de 'styleUrl'
+})
+export class LoginComponent {
+
+}
